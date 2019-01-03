e1acf6cf10795c4d592c3a24c1b93892a329f952
Dima Shumanski <dima.shumanski@gmail.com>
2019-01-03 12:57:31 +0300

    Added Lab_2

---
+++ Lab_2/Lab_2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Lab_2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Lab_2/Lab_2/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Lab_2.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public static int result = 0;
+
+        public static Stack<int> stack = new Stack<int>();
+
+        public string Get()
+        {
+            return JsonConvert.SerializeObject(new
+            {
+                result = result + stack.FirstOrDefault()
+            });
+        }
+
+        public void Post([FromBody]string value)
+        {
+            result = Convert.ToInt32(value);
+        }
+
+        public void Put([FromBody]string value)
+        {
+            stack.Push(Convert.ToInt32(value));
+        }
+
+        public void Delete()
+        {
+            if (stack.Count != 0)
+            {
+                stack.Pop();
+            }
+        }
+    }
+}
