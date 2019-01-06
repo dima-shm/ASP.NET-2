0dd56d4760da378506f187550fdae7a1a21f2d2a
Dima Shumanski <dima.shumanski@gmail.com>
2019-01-07 00:28:11 +0300

    Added Lab_8

---
+++ Lab_8/Lab_8/Controllers/JRServiceController.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Lab_8.Controllers
+{
+    public class JRServiceController : ApiController
+    {
+        private static MyCache cache = new MyCache();
+
+        [HttpPost]
+        public List<JsonRPCResponse> Post(JsonRPCRequest[] request)
+        {
+            List<JsonRPCResponse> response = new List<JsonRPCResponse>();
+            foreach (JsonRPCRequest requestItem in request)
+            {
+                JsonRPCResponse responseItem = null;
+
+                switch (requestItem.Method.ToLower())
+                {
+                    case "setm":
+                        responseItem = SetM(requestItem);
+                        break;
+                    case "getm":
+                        responseItem = GetM(requestItem);
+                        break;
+                    case "addm":
+                        responseItem = AddM(requestItem);
+                        break;
+                    case "subm":
+                        responseItem = SubM(requestItem);
+                        break;
+                    case "mulm":
+                        responseItem = MulM(requestItem);
+                        break;
+                    case "divm":
+                        responseItem = DivM(requestItem);
+                        break;
+                    case "errorexit":
+                        cache.Clear();
+                        return response;
+                    default:
+                        responseItem = methodNotFound(requestItem);
+                        break;
+                }
+                response.Add(responseItem);
+            }
+            return response;
+        }
+
+        [NonAction]
+        private JsonRPCResponse SetM(JsonRPCRequest request)
+        {
+            try
+            {
+                string k = request.Params[0] as string;
+                int x = int.Parse(request.Params[1].ToString());
+                cache.AddValue(k, x);
+                var response = new JsonRPCResponse();
+                response.Id = request.Id;
+                response.JsonRpc = request.JsonRpc;
+                response.Method = request.Method;
+                response.Result = x.ToString();
+                return response;
+            }
+            catch (Exception)
+            {
+                return invalidParams(request);
+            }
+        }
+
+        [NonAction]
+        private JsonRPCResponse GetM(JsonRPCRequest request)
+        {
+            try
+            {
+                var key = request.Params[0] as string;
+                var response = new JsonRPCResponse();
+                response.Id = request.Id;
+                response.JsonRpc = request.JsonRpc;
+                response.Method = request.Method;
+                response.Result = cache.GetValue(key).ToString();
+                return response;
+            }
+            catch (Exception)
+            {
+                return invalidParams(request);
+            }
+        }
+
+        [NonAction]
+        private JsonRPCResponse AddM(JsonRPCRequest request)
+        {
+            try
+            {
+                var response = new JsonRPCResponse();
+                var key = request.Params[0] as string;
+                int value = int.Parse(request.Params[1].ToString());
+                cache.AddValue(key, cache.GetValue(key) + value);
+
+                response.Id = request.Id;
+                response.JsonRpc = request.JsonRpc;
+                response.Method = request.Method;
+                response.Result = cache.GetValue(key).ToString();
+
+                return response;
+            }
+            catch (Exception)
+            {
+                return invalidParams(request);
+            }
+        }
+
+        [NonAction]
+        private JsonRPCResponse SubM(JsonRPCRequest request)
+        {
+            try
+            {
+                var response = new JsonRPCResponse();
+                var key = request.Params[0] as string;
+                int value = int.Parse(request.Params[1].ToString());
+                cache.AddValue(key, cache.GetValue(key) - value);
+
+                response.Id = request.Id;;
+                response.JsonRpc = request.JsonRpc;
+                response.Method = request.Method;
+                response.Result = cache.GetValue(key).ToString();
+
+                return response;
+            }
+            catch (Exception)
+            {
+                return invalidParams(request);
+            }
+        }
+
+        [NonAction]
+        private JsonRPCResponse MulM(JsonRPCRequest request)
+        {
+            try
+            {
+                var response = new JsonRPCResponse();
+                var key = request.Params[0] as string;
+                int value = int.Parse(request.Params[1].ToString());
+                cache.AddValue(key, cache.GetValue(key) * value);
+
+                response.Id = request.Id;
+                response.JsonRpc = request.JsonRpc;
+                response.Method = request.Method;
+                response.Result = cache.GetValue(key).ToString();
+
+                return response;
+            }
+            catch (Exception)
+            {
+                return invalidParams(request);
+            }
+        }
+
+        [NonAction]
+        private JsonRPCResponse DivM(JsonRPCRequest request)
+        {
+            try
+            {
+                var response = new JsonRPCResponse();
+                var key = request.Params[0] as string;
+                int value = int.Parse(request.Params[1].ToString());
+                cache.AddValue(key, cache.GetValue(key) / value);
+
+                response.Id = request.Id;
+                response.JsonRpc = request.JsonRpc;
+                response.Method = request.Method;
+                response.Result = cache.GetValue(key).ToString();
+
+                return response;
+            }
+            catch (Exception)
+            {
+                return invalidParams(request);
+            }
+        }
+
+        [NonAction]
+        private JsonRPCResponse methodNotFound(JsonRPCRequest request)
+        {
+            JsonRPCResponse response = new JsonRPCResponse();
+            response.JsonRpc = request.JsonRpc;
+            response.Id = request.Id;
+            response.Method = request.Method;
+            response.Result = null;
+            JsonRPCError error = new JsonRPCError();
+            error.Code = -32601;
+            error.Message = "Method not found";
+            return response;
+        }
+
+        [NonAction]
+        private JsonRPCResponse invalidParams(JsonRPCRequest request)
+        {
+            JsonRPCResponse response = new JsonRPCResponse();
+            response.JsonRpc = request.JsonRpc;
+            response.Id = request.Id;
+            response.Method = request.Method;
+            response.Result = null;
+            JsonRPCError error = new JsonRPCError();
+            error.Code = -32602;
+            error.Message = "Invalid params";
+            return response;
+        }
+    }
+
+    public class JsonRPCRequest
+    {
+        public string Id { get; set; }
+        public string JsonRpc { get; set; }
+        public string Method { get; set; }
+        public Object[] Params { get; set; }
+    }
+
+    public class JsonRPCResponse
+    {
+        public string Id { get; set; }
+        public string JsonRpc { get; set; }
+        public string Method { get; set; }
+        public string Result { get; set; }
+    }
+
+    public class JsonRPCError
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class MyCache
+    {
+        private Dictionary<String, int> cache = new Dictionary<String, int>();
+
+        public void AddValue(string key, int value)
+        {
+            cache[key] = value;
+        }
+
+        public int GetValue(string key)
+        {
+            try
+            {
+                return cache[key];
+            }
+            catch (KeyNotFoundException)
+            {
+                return 0;
+            }
+        }
+
+        public void Clear()
+        {
+            cache.Clear();
+        }
+    }
+}
---
+++ Lab_8/Lab_8/Controllers/ValuesController.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Http;
+
+namespace Lab_8.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        Dictionary<string, int> valuePairs = new Dictionary<string, int>();
+        bool ErrorMark = false;
+
+        public ValuesController()
+        {
+            HttpContext.Current.Application.Add("VPS", valuePairs);
+            HttpContext.Current.Application.Add("ErrorMark", ErrorMark);
+        }
+
+        [Route("SetMP")]
+        [HttpPost]
+        public ResJsonRPC[] SetM([FromBody] ReqJsonRPC[] jsonreq)
+        {
+            ErrorMark = (bool)HttpContext.Current.Application.Get("ErrorMark");
+            ResJsonRPC[] resJson = new ResJsonRPC[jsonreq.Length];
+            if (!ErrorMark)
+            {
+                for (int i = 0; i < jsonreq.Length; i++)
+                {
+                    resJson[i] = new ResJsonRPC();
+                    valuePairs = (Dictionary<string, int>)HttpContext.Current.Application.Get("VPS");
+                    string k = jsonreq[i].Params[0].ToString();
+                    int x = jsonreq[i].Params[1];
+                    valuePairs[k] = x;
+                    HttpContext.Current.Application.Set("VPS", valuePairs);
+
+                    resJson[i].Id = jsonreq[i].Id;
+                    resJson[i].Jsonrpc = jsonreq[i].Jsonrpc;
+                    resJson[i].Method = jsonreq[i].Method;
+                    resJson[i].Result = x;
+                }
+            }
+            else
+            {
+                resJson[0] = ErrorExit();
+            }
+
+            return resJson;
+        }
+
+        [Route("SetM")]
+        [HttpPost]
+        public ResJsonRPC SetM([FromBody] ReqJsonRPC jsonreq)
+        {
+            ErrorMark = (bool)HttpContext.Current.Application.Get("ErrorMark");
+            ResJsonRPC resJson = new ResJsonRPC();
+            if (!ErrorMark)
+            {
+                valuePairs = (Dictionary<string, int>)HttpContext.Current.Application.Get("VPS");
+                string k = jsonreq.Params[0].ToString();
+                int x = jsonreq.Params[1];
+                valuePairs[k] = x;
+                HttpContext.Current.Application.Set("VPS", valuePairs);
+
+                resJson.Id = jsonreq.Id;
+                resJson.Jsonrpc = jsonreq.Jsonrpc;
+                resJson.Method = jsonreq.Method;
+                resJson.Result = x;
+            }
+            else
+            {
+                resJson = ErrorExit();
+            }
+
+            return resJson;
+        }
+
+        [Route("GetM")]
+        [HttpPost]
+        public ResJsonRPC GetM([FromBody] ReqJsonRPC jsonRPC)
+        {
+            valuePairs = (Dictionary<string, int>)HttpContext.Current.Application.Get("VPS");
+            string k = jsonRPC.Params[0].ToString();
+            ResJsonRPC resJson = new ResJsonRPC();
+            int x = 0;
+            try
+            {
+                x = valuePairs[k];
+                resJson.Id = jsonRPC.Id;
+                resJson.Jsonrpc = jsonRPC.Jsonrpc;
+                resJson.Method = jsonRPC.Method;
+                resJson.Result = x;
+            }
+            catch (Exception)
+            {
+                bool mark = true;
+                HttpContext.Current.Application.Set("ErrorMark", mark);
+                HttpContext.Current.Application.Set("VPS", valuePairs);
+                resJson = ErrorExit();
+            }
+
+            return resJson;
+        }
+
+        [Route("AddMP")]
+        [HttpPost]
+        public ResJsonRPC[] AddM([FromBody] ReqJsonRPC[] jsonreq)
+        {
+            ErrorMark = (bool)HttpContext.Current.Application.Get("ErrorMark");
+            ResJsonRPC[] resJson = new ResJsonRPC[jsonreq.Length];
+            if (!ErrorMark)
+            {
+                for (int i = 0; i < jsonreq.Length; i++)
+                {
+                    resJson[i] = new ResJsonRPC();
+                    valuePairs = (Dictionary<string, int>)HttpContext.Current.Application.Get("VPS");
+                    string k = jsonreq[i].Params[0].ToString();
+                    int x = jsonreq[i].Params[1];
+                    valuePairs[k] += x;
+
+                    HttpContext.Current.Application.Set("VPS", valuePairs);
+                    resJson[i].Id = jsonreq[i].Id;
+                    resJson[i].Jsonrpc = jsonreq[i].Jsonrpc;
+                    resJson[i].Method = jsonreq[i].Method;
+                    resJson[i].Result = valuePairs[k];
+                }
+            }
+            else
+            {
+                resJson[0] = ErrorExit();
+            }
+
+            return resJson;
+        }
+
+        [Route("AddM")]
+        [HttpPost]
+        public ResJsonRPC AddM([FromBody] ReqJsonRPC jsonreq)
+        {
+            ErrorMark = (bool)HttpContext.Current.Application.Get("ErrorMark");
+            ResJsonRPC resJson = new ResJsonRPC();
+            if (!ErrorMark)
+            {
+                valuePairs = (Dictionary<string, int>)HttpContext.Current.Application.Get("VPS");
+                string k = jsonreq.Params[0].ToString();
+                int x = jsonreq.Params[1];
+                valuePairs[k] += x;
+
+                HttpContext.Current.Application.Set("VPS", valuePairs);
+                resJson.Id = jsonreq.Id;
+                resJson.Jsonrpc = jsonreq.Jsonrpc;
+                resJson.Method = jsonreq.Method;
+                resJson.Result = valuePairs[k];
+            }
+            else
+            {
+                resJson = ErrorExit();
+            }
+
+            return resJson;
+        }
+
+        [Route("SubMP")]
+        [HttpPost]
+        public ResJsonRPC[] SubM([FromBody] ReqJsonRPC[] jsonreq)
+        {
+            ErrorMark = (bool)HttpContext.Current.Application.Get("ErrorMark");
+            ResJsonRPC[] resJson = new ResJsonRPC[jsonreq.Length];
+            if (!ErrorMark)
+            {
+                for (int i = 0; i < jsonreq.Length; i++)
+                {
+                    resJson[i] = new ResJsonRPC();
+                    valuePairs = (Dictionary<string, int>)HttpContext.Current.Application.Get("VPS");
+                    string k = jsonreq[i].Params[0].ToString();
+                    int x = jsonreq[i].Params[1];
+                    valuePairs[k] -= x;
+
+                    HttpContext.Current.Application.Set("VPS", valuePairs);
+                    resJson[i].Id = jsonreq[i].Id;
+                    resJson[i].Jsonrpc = jsonreq[i].Jsonrpc;
+                    resJson[i].Method = jsonreq[i].Method;
+                    resJson[i].Result = valuePairs[k];
+                }
+            }
+            else
+            {
+                resJson[0] = ErrorExit();
+            }
+
+            return resJson;
+        }
+
+        [Route("SubM")]
+        [HttpPost]
+        public ResJsonRPC SubM([FromBody] ReqJsonRPC jsonreq)
+        {
+            ErrorMark = (bool)HttpContext.Current.Application.Get("ErrorMark");
+            ResJsonRPC resJson = new ResJsonRPC();
+            if (!ErrorMark)
+            {
+                valuePairs = (Dictionary<string, int>)HttpContext.Current.Application.Get("VPS");
+                string k = jsonreq.Params[0].ToString();
+                int x = jsonreq.Params[1];
+                valuePairs[k] -= x;
+
+                HttpContext.Current.Application.Set("VPS", valuePairs);
+                resJson.Id = jsonreq.Id;
+                resJson.Jsonrpc = jsonreq.Jsonrpc;
+                resJson.Method = jsonreq.Method;
+                resJson.Result = valuePairs[k];
+            }
+            else
+            {
+                resJson = ErrorExit();
+            }
+
+            return resJson;
+        }
+
+        [Route("MulMP")]
+        [HttpPost]
+        public ResJsonRPC[] MulM([FromBody] ReqJsonRPC[] jsonreq)
+        {
+            ErrorMark = (bool)HttpContext.Current.Application.Get("ErrorMark");
+            ResJsonRPC[] resJson = new ResJsonRPC[jsonreq.Length];
+            if (!ErrorMark)
+            {
+                for (int i = 0; i < jsonreq.Length; i++)
+                {
+                    resJson[i] = new ResJsonRPC();
+
+                    valuePairs = (Dictionary<string, int>)HttpContext.Current.Application.Get("VPS");
+                    string k = jsonreq[i].Params[0].ToString();
+                    int x = jsonreq[i].Params[1];
+                    valuePairs[k] *= x;
+
+                    HttpContext.Current.Application.Set("VPS", valuePairs);
+                    resJson[i].Id = jsonreq[i].Id;
+                    resJson[i].Jsonrpc = jsonreq[i].Jsonrpc;
+                    resJson[i].Method = jsonreq[i].Method;
+                    resJson[i].Result = valuePairs[k];
+                }
+            }
+            else
+            {
+                resJson[0] = ErrorExit();
+            }
+
+            return resJson;
+        }
+
+        [Route("MulM")]
+        [HttpPost]
+        public ResJsonRPC MulM([FromBody] ReqJsonRPC jsonreq)
+        {
+            ErrorMark = (bool)HttpContext.Current.Application.Get("ErrorMark");
+            ResJsonRPC resJson = new ResJsonRPC();
+            if (!ErrorMark)
+            {
+                valuePairs = (Dictionary<string, int>)HttpContext.Current.Application.Get("VPS");
+                string k = jsonreq.Params[0].ToString();
+                int x = jsonreq.Params[1];
+                valuePairs[k] *= x;
+
+                HttpContext.Current.Application.Set("VPS", valuePairs);
+                resJson.Id = jsonreq.Id;
+                resJson.Jsonrpc = jsonreq.Jsonrpc;
+                resJson.Method = jsonreq.Method;
+                resJson.Result = valuePairs[k];
+            }
+            else
+            {
+                resJson = ErrorExit();
+            }
+
+            return resJson;
+        }
+
+        [Route("DivMP")]
+        [HttpPost]
+        public ResJsonRPC[] DivM([FromBody] ReqJsonRPC[] jsonreq)
+        {
+            ErrorMark = (bool)HttpContext.Current.Application.Get("ErrorMark");
+            ResJsonRPC[] resJson = new ResJsonRPC[jsonreq.Length];
+            if (!ErrorMark)
+            {
+                for (int i = 0; i < jsonreq.Length; i++)
+                {
+                    resJson[i] = new ResJsonRPC();
+
+                    valuePairs = (Dictionary<string, int>)HttpContext.Current.Application.Get("VPS");
+                    string k = jsonreq[i].Params[0].ToString();
+                    int x = jsonreq[i].Params[1];
+                    valuePairs[k] /= x;
+
+                    HttpContext.Current.Application.Set("VPS", valuePairs);
+                    resJson[i].Id = jsonreq[i].Id;
+                    resJson[i].Jsonrpc = jsonreq[i].Jsonrpc;
+                    resJson[i].Method = jsonreq[i].Method;
+                    resJson[i].Result = valuePairs[k];
+                }
+            }
+            else
+            {
+                resJson[0] = ErrorExit();
+            }
+
+            return resJson;
+        }
+
+        [Route("DivM")]
+        [HttpPost]
+        public ResJsonRPC DivM([FromBody] ReqJsonRPC jsonreq)
+        {
+            ErrorMark = (bool)HttpContext.Current.Application.Get("ErrorMark");
+            ResJsonRPC resJson = new ResJsonRPC();
+            if (!ErrorMark)
+            {
+                valuePairs = (Dictionary<string, int>)HttpContext.Current.Application.Get("VPS");
+                string k = jsonreq.Params[0].ToString();
+                int x = jsonreq.Params[1];
+                valuePairs[k] /= x;
+
+                HttpContext.Current.Application.Set("VPS", valuePairs);
+                resJson.Id = jsonreq.Id;
+                resJson.Jsonrpc = jsonreq.Jsonrpc;
+                resJson.Method = jsonreq.Method;
+                resJson.Result = valuePairs[k];
+            }
+            else
+            {
+                resJson = ErrorExit();
+            }
+
+            return resJson;
+        }
+
+        public ResJsonRPC ErrorExit()
+        {
+            ResJsonRPC resJson = new ResJsonRPC()
+            {
+                Id = "404",
+                Jsonrpc = "2.0",
+                Method = "ErrorExit()",
+                Result = 0
+            };
+
+            return resJson;
+        }
+
+        public class ReqJsonRPC
+        {
+            public string Id { get; set; }
+            public string Jsonrpc { get; set; }
+            public string Method { get; set; }
+            public int[] Params { get; set; }
+        }
+
+        public class ResJsonRPC
+        {
+            public string Id { get; set; }
+            public string Jsonrpc { get; set; }
+            public string Method { get; set; }
+            public int Result { get; set; }
+        }
+    }
+}
